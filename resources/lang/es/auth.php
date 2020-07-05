26de302cf9efefcb6d6c5edbbd2cba0c77026abf
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2020-07-05 08:51:51 -0500

    Poner en español

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'fresh','Un link de verificación fue enviado a tu correo',
+    'Before','Antes de continuar, reviza tu correo, te hemos enviado un link de verificación',
+    'didnotrecive','Si no has recibido el correo',
+    'Please'=>'Por favor confirma tu password antes de continuar.',
+    'Remember'=>'Recordarme',
+    'Reset'=>'Cambiar password',
+    'Password'=>'Contraseña',
+    'Confirm'=>'Confirma tu password',
+    'Forgot'=>'Olvidaste tu password?',
+    'Name'=>'Nombre de usuario',
+    'E-Mail' => 'Correo eléctronico',
+    'Login' => 'Entrar',
+    'Register'     => 'Registrarse',
+    'Logout'=>'Salir',
+    'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
+];
---
+++ resources/lang/es/messages.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'Toggle' =>  'Cambiar navegación',
+];
+
